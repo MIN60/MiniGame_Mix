5f44e0a53c42cecc7b9a23483f49b2c04830d6c3
MIN60 <49427080+MIN60@users.noreply.github.com>
2020-12-19 20:43:55 +0900

    재료혼합
    ㄴ

---
+++ Assets/Script/pic2.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pic2 : MonoBehaviour
+{
+    [SerializeField]
+    private Transform pic2place;
+
+    private Vector2 initialPosition;
+
+    private float deltaX, deltaY;
+
+    public static bool locked;
+
+    private void Start()
+    {
+        initialPosition = transform.position;
+    }
+
+    private void Update()
+    {
+        if (Input.touchCount > 0 && !locked)
+        {
+            Touch touch = Input.GetTouch(0);
+            Vector2 touchPos = Camera.main.ScreenToWorldPoint(touch.position);
+
+            switch (touch.phase)
+            {
+                case TouchPhase.Began:
+                    if (GetComponent<Collider2D>() == Physics2D.OverlapPoint(touchPos))
+                    {
+                        deltaX = touchPos.x - transform.position.x;
+                        deltaY = touchPos.y - transform.position.y;
+                    }
+                    break;
+
+                case TouchPhase.Moved:
+                    if (GetComponent<Collider2D>() == Physics2D.OverlapPoint(touchPos))
+                        transform.position = new Vector2(touchPos.x - deltaX, touchPos.y - deltaY);
+                    break;
+
+                case TouchPhase.Ended:
+                    if (Mathf.Abs(transform.position.x - pic2place.position.x) <= 0.5f &&
+                        Mathf.Abs(transform.position.y - pic2place.position.y) <= 0.5f)
+                    {
+                        transform.position = new Vector2(pic2place.position.x, pic2place.position.y);
+                        locked = true;
+                    }
+                    else
+                    {
+                        transform.position = new Vector2(initialPosition.x, initialPosition.y);
+                    }
+                    break;
+            }
+        }
+    }
+}
